e1a497e3633b2c785ad4b448b24b4c9257f75f13
Ovidiu Ciule <ovidiu.ciule@gmail.com>
2014-06-09 12:35:35 +0200

    Fixed multiplication_tables solution

--- multiplication_tables/Main.java
+++ multiplication_tables/Main.java
@@ -1,15 +1,16 @@
 
 
-public class MultiplicationTables {
+public class Main {
     public static void main (String[] args) {
         int x = 12;
         int y = 12;
 
         for (int i = 1; i<= x; i++) {
+            StringBuffer sb = new StringBuffer();
             for (int j = 1; j<= y; j++) {
-                System.out.format("%4d", i * j);
+                sb.append(String.format("%4d", i * j));
             }
-            System.out.println("");
+            System.out.println(sb.toString().trim());
         }
     }
 }
