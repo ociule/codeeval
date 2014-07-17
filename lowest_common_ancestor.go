c509486bae7eaa16a048c63604f20306b3dd2920
Ovidiu Ciule <ovidiu.ciule@gmail.com>
2014-07-17 17:48:42 +0200

    Go fmt

--- lowest_common_ancestor.go
+++ lowest_common_ancestor.go
@@ -5,53 +5,54 @@ import "log"
 import "bufio"
 import "os"
 import "strings"
+
 //import "math"
 
 type Node struct {
-    Value int
-    Left *Node
-    Right *Node
+	Value int
+	Left  *Node
+	Right *Node
 }
 
 func parse_test(test string) (int, int) {
-    x, y := 0, 0
-    fmt.Sscanf(test, "%d %d", &x, &y)
-    return x, y
+	x, y := 0, 0
+	fmt.Sscanf(test, "%d %d", &x, &y)
+	return x, y
 }
 
-func (root *Node) findByValue(value int) (*Node) {
-    for root != nil {
-        if root.Value == value {
-            return root
-        } else {
-            if root.Value > value {
-                root = root.Left
-            } else {
-                root = root.Right
-            }
-        }
-    }
-    return root
+func (root *Node) findByValue(value int) *Node {
+	for root != nil {
+		if root.Value == value {
+			return root
+		} else {
+			if root.Value > value {
+				root = root.Left
+			} else {
+				root = root.Right
+			}
+		}
+	}
+	return root
 }
 
 func (root *Node) find_lowest_common_ancestor(x, y int) *Node {
-    if (y < root.Value && root.Value < x) || y == root.Value || x == root.Value {
-        return root
-    } else {
-        if y < root.Value {
-            return root.Left.find_lowest_common_ancestor(x, y)
-        } else {
-            return root.Right.find_lowest_common_ancestor(x, y)
-        }
-    }
+	if (y < root.Value && root.Value < x) || y == root.Value || x == root.Value {
+		return root
+	} else {
+		if y < root.Value {
+			return root.Left.find_lowest_common_ancestor(x, y)
+		} else {
+			return root.Right.find_lowest_common_ancestor(x, y)
+		}
+	}
 }
 
 func max_min(x, y int) (int, int) {
-    if x > y {
-        return x, y
-    } else {
-        return y, x
-    }
+	if x > y {
+		return x, y
+	} else {
+		return y, x
+	}
 }
 
 func main() {
@@ -62,17 +63,17 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-    tree := Node{30,
-        &Node{8,
-            &Node{3, nil, nil},
-            &Node{20, &Node{10, nil, nil}, &Node{29, nil, nil}}},
-        &Node{52, nil, nil}}
+	tree := Node{30,
+		&Node{8,
+			&Node{3, nil, nil},
+			&Node{20, &Node{10, nil, nil}, &Node{29, nil, nil}}},
+		&Node{52, nil, nil}}
 
 	for scanner.Scan() {
-        line := strings.TrimSpace(scanner.Text())
-        //fmt.Println(line)
+		line := strings.TrimSpace(scanner.Text())
+		//fmt.Println(line)
 		x, y := parse_test(line)
-        x, y = max_min(x, y)
+		x, y = max_min(x, y)
 		fmt.Println(tree.find_lowest_common_ancestor(x, y).Value)
 	}
 }
--- n_mod_m.go
+++ n_mod_m.go
@@ -9,10 +9,10 @@ import "strconv"
 
 // N mod M
 func mod(n, m int64) int64 {
-    for n >= m {
-        n = n - m
-    }
-    return n
+	for n >= m {
+		n = n - m
+	}
+	return n
 }
 
 func main() {
@@ -26,8 +26,8 @@ func main() {
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 		lineSplit := strings.Split(line, ",")
-        n, _ := strconv.ParseInt(lineSplit[0], 10, 8)
-        m, _ := strconv.ParseInt(lineSplit[1], 10, 8)
+		n, _ := strconv.ParseInt(lineSplit[0], 10, 8)
+		m, _ := strconv.ParseInt(lineSplit[1], 10, 8)
 		fmt.Println(mod(n, m))
 	}
 }
