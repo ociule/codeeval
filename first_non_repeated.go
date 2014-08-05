3afb442c858f843da88859713726a18532415a85
Ovidiu Ciule <ovidiu.ciule@gmail.com>
2014-08-05 19:41:43 +0200

    go fmt

--- first_non_repeated.go
+++ first_non_repeated.go
@@ -7,31 +7,31 @@ import "os"
 import "strings"
 
 type RunePosCount struct {
-    Pos, Count int
+	Pos, Count int
 }
 
 func getFirst(in string) string {
-    nonRepeated := make(map[rune]RunePosCount, len(in))
-    for ix, char := range in {
-        val, ok := nonRepeated[char]
-        if !ok {
-            nonRepeated[char] = RunePosCount{ix, 1}
-        } else {
-            nonRepeated[char] = RunePosCount{val.Pos, val.Count + 1}
-            //fmt.Println(string(char), ix, val.Count + 1)
-        }
-    }
+	nonRepeated := make(map[rune]RunePosCount, len(in))
+	for ix, char := range in {
+		val, ok := nonRepeated[char]
+		if !ok {
+			nonRepeated[char] = RunePosCount{ix, 1}
+		} else {
+			nonRepeated[char] = RunePosCount{val.Pos, val.Count + 1}
+			//fmt.Println(string(char), ix, val.Count + 1)
+		}
+	}
 
-    //fmt.Println(nonRepeated)
-    lowestPos := len(in)
-    out := rune(0)
-    for char, posCount := range nonRepeated {
-        if posCount.Count == 1 && posCount.Pos < lowestPos {
-            out = char
-            lowestPos = posCount.Pos
-        }
-    }
-    return string(out)
+	//fmt.Println(nonRepeated)
+	lowestPos := len(in)
+	out := rune(0)
+	for char, posCount := range nonRepeated {
+		if posCount.Count == 1 && posCount.Pos < lowestPos {
+			out = char
+			lowestPos = posCount.Pos
+		}
+	}
+	return string(out)
 }
 
 func main() {
